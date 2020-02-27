d6b932d7f727be9cfba0aaa4d75ee9aa872ce0eb
Chris Kettenbach <christopher.kettenbach@primitivelogic.com>
2020-02-26 20:03:21 -0800

    second commit

---
+++ src/models/user.rs
@@ -0,0 +1,108 @@
+use crate::schema::users;
+use chrono::NaiveDateTime;
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "users"]
+pub struct User {
+    #[serde(skip)]
+    pub id: i32,
+    pub email: String,
+    pub company: String,
+    #[serde(skip)]
+    pub password: String,
+    pub created_at: NaiveDateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize, Insertable)]
+#[table_name = "users"]
+pub struct NewUser {
+    pub email: String,
+    pub company: String,
+    pub password: String,
+    pub created_at: NaiveDateTime,
+}
+
+use crate::errors::MyStoreError;
+use bcrypt::{hash, DEFAULT_COST};
+use chrono::Local;
+use diesel::PgConnection;
+
+impl User {
+    pub fn create(
+        register_user: RegisterUser,
+        connection: &PgConnection,
+    ) -> Result<User, MyStoreError> {
+        use diesel::RunQueryDsl;
+
+        Ok(diesel::insert_into(users::table)
+            .values(NewUser {
+                email: register_user.email,
+                company: register_user.company,
+                password: Self::hash_password(register_user.password)?,
+                created_at: Local::now().naive_local(),
+            })
+            .get_result(connection)?)
+    }
+
+    pub fn hash_password(plain: String) -> Result<String, MyStoreError> {
+        Ok(hash(plain, DEFAULT_COST)?)
+    }
+}
+
+#[derive(Deserialize)]
+pub struct RegisterUser {
+    pub email: String,
+    pub company: String,
+    pub password: String,
+    pub password_confirmation: String,
+}
+
+impl RegisterUser {
+    pub fn validates(self) -> Result<RegisterUser, MyStoreError> {
+        let password_are_equal = self.password == self.password_confirmation;
+        let password_not_empty = self.password.len() > 0;
+        if password_are_equal && password_not_empty {
+            Ok(self)
+        } else if !password_are_equal {
+            Err(MyStoreError::PasswordNotMatch(
+                "Password and Password Confirmation does not match".to_string(),
+            ))
+        } else {
+            Err(MyStoreError::WrongPassword(
+                "Wrong Password, check it is not empty".to_string(),
+            ))
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct AuthUser {
+    pub email: String,
+    pub password: String,
+}
+
+impl AuthUser {
+    pub fn login(&self, connection: &PgConnection) -> Result<User, MyStoreError> {
+        use crate::schema::users::dsl::email;
+        use bcrypt::verify;
+        use diesel::ExpressionMethods;
+        use diesel::QueryDsl;
+        use diesel::RunQueryDsl;
+
+        let mut records = users::table
+            .filter(email.eq(&self.email))
+            .load::<User>(connection)?;
+
+        let user = records
+            .pop()
+            .ok_or(MyStoreError::DBError(diesel::result::Error::NotFound))?;
+
+        if verify(&self.password, &user.password)? {
+            Ok(user)
+        } else {
+            Err(MyStoreError::WrongPassword(
+                "Wrong password, check again please".to_string(),
+            ))
+        }
+    }
+}
--- src/schema.rs
+++ src/schema.rs
@@ -6,3 +6,18 @@ table! {
         published -> Bool,
     }
 }
+
+table! {
+    users (id) {
+        id -> Int4,
+        email -> Varchar,
+        company -> Varchar,
+        password -> Varchar,
+        created_at -> Timestamp,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
